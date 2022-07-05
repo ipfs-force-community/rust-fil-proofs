c4ec114d8c6bf783cffa6a299a637a05c9c8be72
0x5459 <0x5459@protonmail.com>
2022-07-05 23:15:39 +0800

    feat(memory): make the setup_create_label_memory function support acquire memory from shm
    * add `multicore_sdr_shm_numa_dir_pattern` field to `storage_proofs_core::settings Settings`
    * add `numa_pool()` to get the static global NUMA pool reference
    * `setup_create_label_memory` will mapping memory from SHM first
    * uinttests for  `scan_shm_files` and `numa_pool`

---
+++ storage-proofs-porep/src/stacked/vanilla/memory_handling/shm.rs
@@ -0,0 +1,220 @@
+use std::{
+    collections::HashMap,
+    fs::OpenOptions,
+    path::Path,
+    sync::{Mutex, MutexGuard},
+};
+
+use log::{info, warn};
+use mapr::{MmapMut, MmapOptions};
+
+use crate::stacked::vanilla::numa::NumaNodeIndex;
+
+// memory_size -> memory
+type ShmPool = HashMap<usize, Vec<Mutex<MmapMut>>>;
+
+pub(super) struct NumaPool {
+    /// The index of the numa_groups vec is numa_node_index
+    numa_groups: Vec<ShmPool>,
+}
+
+impl NumaPool {
+    /// Create an empty NumaPoll
+    pub fn empty() -> Self {
+        Self {
+            numa_groups: Vec::new(),
+        }
+    }
+
+    /// Create NumaPool with the given numa_shm_files
+    ///
+    /// The index of the numa_shm_files vec is numa_node_index,
+    /// and each item of numa_shm_files is the shm file paths corresponding to numa_node_index
+    pub fn new(numa_shm_files: Vec<impl IntoIterator<Item = impl AsRef<Path>>>) -> Self {
+        let numa_groups = numa_shm_files
+            .into_iter()
+            .map(Self::load_shm_files)
+            .collect();
+        Self { numa_groups }
+    }
+
+    fn load_shm_files(shm_files: impl IntoIterator<Item = impl AsRef<Path>>) -> ShmPool {
+        let mut shm_pool: HashMap<usize, Vec<Mutex<MmapMut>>> = HashMap::new();
+
+        for p in shm_files.into_iter() {
+            let p = p.as_ref();
+            let shm_file = match OpenOptions::new().read(true).write(true).open(p) {
+                Ok(file) => file,
+                Err(e) => {
+                    warn!(
+                        "open shm file: '{}', {:?}. ignore this shm file.",
+                        p.display(),
+                        e
+                    );
+                    continue;
+                }
+            };
+
+            let file_size = match shm_file.metadata() {
+                Ok(meta) => meta.len(),
+                Err(e) => {
+                    warn!(
+                        "get the size of the '{}' file: {:?}. ignore this shm file.",
+                        p.display(),
+                        e,
+                    );
+                    continue;
+                }
+            };
+
+            let mmap = match unsafe { MmapOptions::new().lock().map_mut(&shm_file) } {
+                Ok(mmap) => mmap,
+                Err(e) => {
+                    // fallback to not locked if permissions are not available
+                    warn!(
+                        "lock mmap shm file '{}': {:?}. falling back",
+                        p.display(),
+                        e
+                    );
+                    match unsafe { MmapOptions::new().map_mut(&shm_file) } {
+                        Ok(mmap) => mmap,
+                        Err(e) => {
+                            warn!(
+                                "mmap shm file '{}': {:?}. ignore this shm file.",
+                                p.display(),
+                                e
+                            );
+                            continue;
+                        }
+                    }
+                }
+            };
+            info!("loaded shm file: {}", p.display());
+            let mmap = Mutex::new(mmap);
+            shm_pool
+                .entry(file_size as usize)
+                .or_insert_with(|| vec![])
+                .push(mmap);
+        }
+        shm_pool
+    }
+
+    /// Acquire the shm memory for the specified size in the specified numa node
+    ///
+    /// Make sure that the thread calling this function and the thread that using
+    /// the memory returned by this function are in the same numa node and make sure
+    /// the thread that using the returned memory will not be dispatched to other numa nodes,
+    /// otherwise the performance of using returned memory will be very low
+    pub fn acquire(&self, size: usize) -> Option<MutexGuard<'_, MmapMut>> {
+        let numa_group = self
+            .numa_groups
+            .get(current_numa_node().unwrap_or_default().raw() as usize)?;
+        for l in numa_group.get(&size)? {
+            match l.try_lock() {
+                Ok(m) => return Some(m),
+                Err(_) => {}
+            }
+        }
+        None
+    }
+}
+
+#[cfg(not(test))]
+fn current_numa_node() -> Option<NumaNodeIndex> {
+    use crate::stacked::vanilla::numa;
+    numa::current_numa_node()
+}
+
+#[cfg(test)]
+fn current_numa_node() -> Option<NumaNodeIndex> {
+    unsafe { tests::CUR_NUMA_NODE.lock().unwrap().clone() }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs;
+    use std::sync::Mutex;
+
+    use crate::stacked::vanilla::numa::NumaNodeIndex;
+
+    use super::NumaPool;
+
+    pub(super) static mut CUR_NUMA_NODE: Mutex<Option<NumaNodeIndex>> = Mutex::new(None);
+
+    /// set current numa node for testing
+    fn set_current_numa_node(curr: Option<NumaNodeIndex>) {
+        unsafe {
+            *CUR_NUMA_NODE.lock().unwrap() = curr;
+        }
+    }
+
+    #[test]
+    fn test_numa_pool() {
+        let temp_dir = tempfile::tempdir().expect("Failed to create tempdir");
+        let temp_dir_path = temp_dir.as_ref();
+
+        fn size_fn(numa_node_idx: usize) -> usize {
+            (numa_node_idx + 1) * 10
+        }
+
+        let numa_shm_files: Vec<_> = (0..2)
+            .map(|numa_node_idx| {
+                (0..2).map(move |i| {
+                    let path = temp_dir_path.join(format!("numa_{}_{}", numa_node_idx, i));
+
+                    fs::write(&path, " ".repeat(size_fn(numa_node_idx)))
+                        .expect("Failed to write data");
+                    path
+                })
+            })
+            .collect();
+        let numa_pool = NumaPool::new(numa_shm_files);
+
+        let mut mems = Vec::new();
+        for numa_node_idx in 0..2 {
+            let size = size_fn(numa_node_idx);
+            let no_exist_size = size + 1;
+
+            set_current_numa_node(Some(NumaNodeIndex::new(numa_node_idx as u32)));
+
+            for _ in 0..2 {
+                // Test for normal memory acquire
+                let mem = numa_pool.acquire(size);
+                assert!(mem.is_some());
+                mems.push(mem);
+
+                // Test to acquire the shared memory of non-existent memory size
+                assert!(
+                    numa_pool.acquire(no_exist_size).is_none(),
+                    "acquire non-existent memory size should return None"
+                );
+            }
+        }
+
+        // Test when NumaPool is empty
+        for numa_node_idx in 0..2 {
+            let size = size_fn(numa_node_idx);
+            set_current_numa_node(Some(NumaNodeIndex::new(numa_node_idx as u32)));
+
+            for _ in 0..2 {
+                assert!(
+                    numa_pool.acquire(size).is_none(),
+                    "acquire memory from empty NumaPool should return None"
+                );
+            }
+        }
+
+        drop(mems);
+
+        for numa_node_idx in 0..2 {
+            let size = size_fn(numa_node_idx);
+            set_current_numa_node(Some(NumaNodeIndex::new(numa_node_idx as u32)));
+
+            for _ in 0..2 {
+                // Test for normal memory acquire
+                let mem = numa_pool.acquire(size);
+                assert!(mem.is_some());
+            }
+        }
+    }
+}
--- storage-proofs-porep/src/stacked/vanilla/numa.rs
+++ storage-proofs-porep/src/stacked/vanilla/numa.rs
@@ -1,47 +1,76 @@
-use libc::{self, c_int};
+#[cfg(target_os = "linux")]
+pub use linux::*;
 
-#[link(name = "numa", kind = "dylib")]
-extern "C" {
-    /// Check if NUMA support is enabled. Returns -1 if not enabled, in which case other functions will undefined
-    fn numa_available() -> c_int;
-    ///  Returns the NUMA node corresponding to a CPU, or -1 if the CPU is invalid
-    fn numa_node_of_cpu(cpu: c_int) -> c_int;
-}
+#[cfg(not(target_os = "linux"))]
+pub use unsupported::*;
 
-lazy_static! {
-    static ref NUMA_AVAILABLE: bool = unsafe { numa_available() >= 0 };
-}
+use libc::c_int;
 
 /// Index of a NUMA node.
-#[derive(Clone, Copy, Debug, PartialEq, PartialOrd, Eq, Ord, Hash)]
+#[derive(Clone, Copy, Debug, PartialEq, PartialOrd, Eq, Ord, Hash, Default)]
 pub struct NumaNodeIndex(u32);
 
-impl NumaNodeIndex(u32) {
+impl NumaNodeIndex {
+    pub fn new(idx: u32) -> Self {
+        Self(idx)
+    }
+
     /// Returns NUMA node index of c_int type
     pub fn raw(&self) -> c_int {
         self.0 as c_int
     }
 }
 
-/// Returns the current NUMA node on which the thread is running.
-///
-/// Since threads may migrate to another node with the scheduler,
-/// you need to bind the current worker thread to the specified core when calling this function
-pub fn current_node() -> NumaNodeIndex {
-    unsafe {
+#[cfg(target_os = "linux")]
+pub mod linux {
+
+    use lazy_static::lazy_static;
+    use libc::{self, c_int};
+
+    use super::NumaNodeIndex;
+
+    #[link(name = "numa", kind = "dylib")]
+    extern "C" {
+        /// Check if NUMA support is enabled. Returns -1 if not enabled, in which case other functions will undefined
+        fn numa_available() -> c_int;
+        ///  Returns the NUMA node corresponding to a CPU core, or -1 if the CPU core is invalid
+        fn numa_node_of_cpu(cpu: c_int) -> c_int;
+    }
+
+    lazy_static! {
+        static ref NUMA_AVAILABLE: bool = unsafe { numa_available() >= 0 };
+    }
+
+    /// Returns the current NUMA node on which the thread is running.
+    ///
+    /// Since threads may migrate to another node with the scheduler,
+    /// you need to bind the current worker thread to the specified core when calling this function
+    #[allow(dead_code)]
+    pub fn current_numa_node() -> Option<NumaNodeIndex> {
         if !*NUMA_AVAILABLE {
-            return NumaNodeIndex(0);
+            return None;
         }
-        let cpu = libc::sched_getcpu();
-        // Use 0 as fallback if sched_getcpu call fails
+        let cpu = unsafe { libc::sched_getcpu() };
+        // Return None if sched_getcpu call fails
         if cpu < 0 {
-            return NumaNodeIndex(0);
+            return None;
         }
         let node = unsafe { numa_node_of_cpu(cpu) };
-        // If libnuma cannot find the appropriate node, then use 0 as fallback.
+        // If libnuma cannot find the appropriate node, then return None
         if node < 0 {
-            return NumaNodeIndex(0);
+            return None;
         }
-        NumaNodeIndex(node as u32)
+        Some(NumaNodeIndex(node as u32))
+    }
+}
+
+#[cfg(not(target_os = "linux"))]
+mod unsupported {
+
+    use super::NumaNodeIndex;
+
+    #[allow(dead_code)]
+    pub fn current_numa_node() -> Option<NumaNodeIndex> {
+        None
     }
 }
